de00b5b9744afc4a1b528ffd7fd54f93e0ae0672
bradley.j.fitzpatrick <bradley.j.fitzpatrick@ad9c9841-ba53-0410-b846-bb8d69ff4585>
2008-08-05 19:11:09 +0000

    start of async urlfetch stuff
    git-svn-id: http://pubsubhubbub.googlecode.com/svn/trunk@3 ad9c9841-ba53-0410-b846-bb8d69ff4585

--- main.py
+++ main.py
@@ -15,25 +15,66 @@
 # limitations under the License.
 #
 
-
-
-
 import wsgiref.handlers
 
-
+from google.appengine.api import urlfetch
+from google.appengine.api import apiproxy_stub_map
+from google.appengine.api import urlfetch_service_pb
+from google.appengine.api.urlfetch_errors import *
 from google.appengine.ext import webapp
+from google.appengine.runtime import apiproxy_errors
 
+import urlfetch_async
+
+
+class AsyncRunner:
+  def __init__(self):
+    self.enqueued = []
+
+  def start_call(self, service, method, pbrequest, pbresponse, callback):
+    """Callback is ->(pbresponse | None, None | Exception)"""
+    self.enqueued.append((service, method, pbrequest, pbresponse, callback))
+
+  def rpcs_outstanding(self):
+    return len(self.enqueued);
+
+  def wait(self):
+    """Wait for RPCs to finish.  Returns true if one was processed."""
+    if not self.rpcs_outstanding():
+      return False
+    (service, method, pbrequest, pbresponse, callback) = self.enqueued.pop(0);
+    try:
+      apiproxy_stub_map.MakeSyncCall(service, method, pbrequest, pbresponse)
+      callback(pbresponse, None)
+    except apiproxy_errors.ApplicationError, e:
+      callback(None, e)
+    return True
+
+async_runner = AsyncRunner()
 
 class MainHandler(webapp.RequestHandler):
 
   def get(self):
     self.response.out.write('Hello world!')
+    # start async fetch:
+    urlfetch_async.fetch("http://bradfitz.com/", async_runner=async_runner, callback=self.on_url)
+    async_runner.wait()
 
+  def on_url(self, response, exception):
+    # response = urlfetch_service_pb.URLFetchResponse
+    if response:
+      self.response.out.write("Got content: " + response.content())
+    else:
+      self.response.out.write("Got exception!")
+
+
+  
 
 def main():
   application = webapp.WSGIApplication([('/', MainHandler)],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
+
 
 
 if __name__ == '__main__':
