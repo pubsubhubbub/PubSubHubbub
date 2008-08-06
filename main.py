fd97c0eff56f9d0a0ca1b4ec35ecec0b932f49b8
bslatkin <bslatkin@ad9c9841-ba53-0410-b846-bb8d69ff4585>
2008-08-06 08:55:43 +0000

    Subscribe/Unsubscribe state transitions for sync/async modes.
    git-svn-id: http://pubsubhubbub.googlecode.com/svn/trunk@14 ad9c9841-ba53-0410-b846-bb8d69ff4585

--- main.py
+++ main.py
@@ -16,6 +16,7 @@
 #
 
 import datetime
+import hashlib
 import logging
 logging.getLogger().setLevel(logging.DEBUG)
 import wsgiref.handlers
@@ -39,12 +40,136 @@ EXPIRATION_DELTA = datetime.timedelta(days=90)
 
 
 class Subscription(db.Model):
-  # TODO: Handle 2000 byte URLs by indexing on a hash property and then having
-  # a TextProperty with the whole callback_url inside of it.
-  callback_url = db.StringProperty(required=True)
+  """Represents a single subscription to a topic for a callback URL."""
+
+  STATE_NOT_VERIFIED = 'not_verified'
+  STATE_VERIFIED = 'verified'
+  STATE_TO_DELETE = 'to_delete'
+  STATES = frozenset([
+    STATE_NOT_VERIFIED,
+    STATE_VERIFIED,
+    STATE_TO_DELETE,
+  ])
+
+  callback_url = db.TextProperty(required=True)
   topic = db.StringProperty(required=True)
+  created_time = db.DateTimeProperty(auto_now_add=True)
   expiration_time = db.DateTimeProperty(required=True)
-  has_been_verified = db.BooleanProperty(required=True)
+  subscription_state = db.StringProperty(default=STATE_NOT_VERIFIED)
+
+  @staticmethod
+  def create_key_name(callback_url, topic):
+    combined_id = '%s\n%s' % (callback_url, topic)
+    return 'hash_' + hashlib.sha1(combined_id).hexdigest()
+
+  @classmethod
+  def insert(cls, callback_url, topic):
+    """Marks a callback URL as being subscribed to a topic.
+    
+    Creates a new subscription if None already exists. Forces any existing,
+    pending request (i.e., async) to immediately enter the verified state.
+    
+    Args:
+      callback_url: URL that will receive callbacks.
+      topic: The topic to subscribe to.
+    
+    Returns:
+      True if the subscription was newly created, False otherwise.
+    """
+    key_name = cls.create_key_name(callback_url, topic)
+    def txn():
+      sub_is_new = False
+      sub = cls.get_by_key_name(key_name)
+      if sub is None:
+        sub_is_new = True
+        sub = cls(key_name=key_name,
+                  callback_url=callback_url,
+                  topic=topic,
+                  expiration_time=datetime.datetime.now() + EXPIRATION_DELTA)
+      sub.subscription_state = cls.STATE_VERIFIED
+      sub.put()
+      return sub_is_new
+    return db.run_in_transaction(txn)
+
+  @classmethod
+  def request_insert(cls, callback_url, topic, **kwargs):
+    """Records that a callback URL needs verification before being subscribed.
+    
+    Creates a new subscription request (for asynchronous verification) if None
+    already exists. Any existing subscription request will not be modified;
+    for instance, if a subscription has already been verified, this method
+    will do nothing.
+    
+    Args:
+      callback_url: URL that will receive callbacks.
+      topic: The topic to subscribe to.
+    
+    Returns:
+      True if the subscription request was newly created, False otherwise.
+    """
+    key_name = cls.create_key_name(callback_url, topic)
+    def txn():
+      sub_is_new = False
+      sub = cls.get_by_key_name(key_name)
+      if sub is None:
+        sub_is_new = True
+        sub = cls(key_name=key_name,
+                  callback_url=callback_url,
+                  topic=topic,
+                  expiration_time=datetime.datetime.now() + EXPIRATION_DELTA,
+                  **kwargs)
+        sub.put()
+      return sub_is_new
+    return db.run_in_transaction(txn)
+
+  @classmethod
+  def remove(cls, callback_url, topic):
+    """Causes a callback URL to no longer be subscribed to a topic.
+    
+    If the callback_url was not already subscribed to the topic, this method
+    will do nothing. Otherwise, the subscription will immediately be removed.
+    
+    Args:
+      callback_url: URL that will receive callbacks.
+      topic: The topic to subscribe to.
+    
+    Returns:
+      True if the subscription had previously existed, False otherwise.
+    """
+    key_name = cls.create_key_name(callback_url, topic)
+    def txn():
+      sub = cls.get_by_key_name(key_name)
+      if sub is not None:
+        sub.delete()
+        return True
+      return False
+    return db.run_in_transaction(txn)
+
+  @classmethod
+  def request_remove(cls, callback_url, topic):
+    """Records that a callback URL needs to be unsubscribed.
+    
+    Creates a new request to unsubscribe a callback URL from a topic (where
+    verification should happen asynchronously). If an unsubscribe request
+    has already been made, this method will do nothing.
+    
+    Args:
+      callback_url: URL that will receive callbacks.
+      topic: The topic to subscribe to.
+    
+    Returns:
+      True if the unsubscribe request is new, False otherwise (i.e., a request
+      for asynchronous unsubscribe was already made).
+    """
+    key_name = cls.create_key_name(callback_url, topic)
+    def txn():
+      sub = cls.get_by_key_name(key_name)
+      if sub is not None and sub.subscription_state != cls.STATE_TO_DELETE:
+        sub.subscription_state = cls.STATE_TO_DELETE
+        sub.put()
+        return True
+      return False
+    return db.run_in_transaction(txn)
 
 
 class SubscribeHandler(webapp.RequestHandler):
@@ -52,27 +177,39 @@ class SubscribeHandler(webapp.RequestHandler):
     self.response.out.write(template.render('subscribe_debug.html', {}))
   
   def post(self):
-    callback = self.request.get('callback')
-    topic = self.request.get('topic')
-    async = self.request.get('async')
+    callback = self.request.get('callback', '').lower()
+    topic = self.request.get('topic', '').lower()
+    async = self.request.get('async', '').lower()
+    mode = self.request.get('mode', '').lower()
     # TODO: Error handling, input validation
-    if not (callback and topic and async):
+    if not (callback and topic and async and mode):
       return self.error(500)
     
     # TODO: Verify the callback
 
-    expiration_time = datetime.datetime.now() + EXPIRATION_DELTA
-    sub = Subscription(
-      callback_url=callback,
-      topic=topic,
-      expiration_time=expiration_time,
-      has_been_verified=True)
-    sub.put()  # TODO exception handling
-    
-    
-    
-      
-    
+    # TODO: exception handling
+    if mode == 'subscribe':
+      if async.startswith('s'):
+        if Subscription.insert(callback, topic):
+          self.response.out.write('Sync: Created subscription.')
+        else:
+          self.response.out.write('Sync: Subscription exists.')
+      else:
+        if Subscription.request_insert(callback, topic):
+          self.response.out.write('Async: Created subscribe request.')
+        else:
+          self.response.out.write('Async: Subscribe request unnecessary.')
+    else:
+      if async.startswith('s'):
+        if Subscription.remove(callback, topic):
+          self.response.out.write('Sync: Removed subscription.')
+        else:
+          self.response.out.write('Sync: No subscription to remove.')
+      else:
+        if Subscription.request_remove(callback, topic):
+          self.response.out.write('Async: Created unsubscribe request.')
+        else:
+          self.response.out.write('Async: Unsubscribe request unnecessary.')
 
 
 class UrlFetchTestHandler(webapp.RequestHandler):
