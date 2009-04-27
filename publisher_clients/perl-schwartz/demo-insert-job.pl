b8b205de8b7d930d0a69b1732ad5dc5b88ab73ef
bradfitz <bradfitz@ad9c9841-ba53-0410-b846-bb8d69ff4585>
2009-04-27 02:25:20 +0000

    add option to choose how many to insert.
    git-svn-id: http://pubsubhubbub.googlecode.com/svn/trunk@104 ad9c9841-ba53-0410-b846-bb8d69ff4585

--- publisher_clients/perl-schwartz/demo-insert-job.pl
+++ publisher_clients/perl-schwartz/demo-insert-job.pl
@@ -2,24 +2,33 @@
 
 use strict;
 use TheSchwartz;
+use Getopt::Long;
+
+my $n_jobs = 1;
+my $hub_url = "http://pubsubhubbub.appspot.com/publish";
+
+GetOptions("n=i"   => \$n_jobs,
+           "hub=s" => \$hub_url,
+           ) or die "Unknown options.";
 
 my $client = TheSchwartz->new(databases => [{
     user => "root",
     dsn => "dbi:mysql:theschwartz",
 }]);
 
-my $hub_url = "http://pubsubhubbub.appspot.com/publish";
+for (1..$n_jobs) {
+    my $topic = "http://publisher.example.com/topic/" . rand() . ".atom";
+    print "Submitting dummy topic $topic ...\n";
+    
+    my $job = TheSchwartz::Job->new(funcname => 'TheSchwartz::Worker::PubSubHubbubPublish',
+                                    arg => {
+                                        hub => $hub_url,
+                                        topic_url => $topic,
+                                    },
+                                    coalesce => $hub_url,
+                                    );
 
-my $topic = "http://publisher.example.com/topic/" . rand() . ".atom";
-print "Submitting dummy topic $topic ...\n";
+    my $handle = $client->insert($job);
+    print "  job handle: $handle\n";
+}
 
-my $job = TheSchwartz::Job->new(funcname => 'TheSchwartz::Worker::PubSubHubbubPublish',
-                                arg => {
-                                    hub => $hub_url,
-                                    topic_url => $topic,
-                                },
-                                coalesce => $hub_url,
-                                );
-
-my $handle = $client->insert($job);
-print "  job handle: $handle\n";
