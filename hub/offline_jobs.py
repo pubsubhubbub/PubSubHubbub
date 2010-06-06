aab5d69ddc33f52e9a4bd803e92f976223903d50
bslatkin <bslatkin@ad9c9841-ba53-0410-b846-bb8d69ff4585>
2010-06-06 01:18:50 +0000

    hub using mapper framework for FeedEntryRecord migration, EventToDeliver cleanup
    git-svn-id: http://pubsubhubbub.googlecode.com/svn/trunk@366 ad9c9841-ba53-0410-b846-bb8d69ff4585

---
+++ hub/offline_jobs.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+#
+# Copyright 2010 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Offline analysis jobs used with the hub."""
+
+import datetime
+import logging
+import time
+
+from google.appengine.ext import db
+
+from mapreduce import context
+from mapreduce import operation as op
+
+
+def RemoveOldFeedEntryRecordPropertiesMapper(feed_entry_record):
+  """Removes old properties from FeedEntryRecord instances."""
+  OLD_PROPERTIES = (
+      'entry_id_hash',
+      'entry_id')
+  for name in OLD_PROPERTIES:
+    if hasattr(feed_entry_record, name):
+      delattr(feed_entry_record, name)
+  yield op.db.Put(feed_entry_record)
+
+
+class CleanupOldEventToDeliver(object):
+  """Removes EventToDeliver instances older than a certain value."""
+
+  @staticmethod
+  def validate_params(params):
+    assert 'age_days' in params
+    params['oldest_last_modified'] = (
+        time.time() - (86400 * int(params['age_days'])))
+
+  def __init__(self):
+    self.oldest_last_modified = None
+
+  def run(self, event):
+    if not self.oldest_last_modified:
+      params = context.get().mapreduce_spec.mapper.params
+      self.oldest_last_modified = datetime.datetime.utcfromtimestamp(
+          params['oldest_last_modified'])
+
+    if event.last_modified < self.oldest_last_modified:
+      yield op.db.Delete(event)
